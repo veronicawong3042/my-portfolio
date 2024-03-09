f0b83bd91e969d43d555fb1189f9f4535520a91f
veronicawong3042 <150846086+veronicawong3042@users.noreply.github.com>
2024-03-08 16:45:26 -0800

    styled about page

--- portfolio-react-app/src/components/AboutModal.jsx
+++ portfolio-react-app/src/components/AboutModal.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import Loading from './Loading';
 import { IoClose } from 'react-icons/io5';
+import { IoIosArrowDown } from "react-icons/io";
 
 const AboutModal = ({ onClose }) => {
     const restBase = 'https://veronica-wong.com/portfolio/wp-json/wp/v2/';
@@ -60,7 +61,10 @@ const AboutModal = ({ onClose }) => {
                                     <li className='stack-heading' onClick={() => updateTab(2)}>{aboutData.acf.stack_heading}</li>
                                     <li className='faq-heading' onClick={() => updateTab(3)}>{aboutData.acf.faq_heading}</li>
                                 </ul>
-                                <h1>{aboutData.acf.about_heading}</h1>
+                                <div className='about-heading-container'>
+                                    <svg className='search-icon' clip-rule="evenodd" fill-rule="evenodd" stroke-linejoin="round" stroke-miterlimit="2" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="m15.97 17.031c-1.479 1.238-3.384 1.985-5.461 1.985-4.697 0-8.509-3.812-8.509-8.508s3.812-8.508 8.509-8.508c4.695 0 8.508 3.812 8.508 8.508 0 2.078-.747 3.984-1.985 5.461l4.749 4.75c.146.146.219.338.219.531 0 .587-.537.75-.75.75-.192 0-.384-.073-.531-.22zm-5.461-13.53c-3.868 0-7.007 3.14-7.007 7.007s3.139 7.007 7.007 7.007c3.866 0 7.007-3.14 7.007-7.007s-3.141-7.007-7.007-7.007z" fill-rule="nonzero"/></svg>
+                                    <h1>{aboutData.acf.about_heading}</h1>
+                                </div>
                                 <div className={tab === 1 ? "show-tab" : "hide-tab"}>
                                     <p>{aboutData.acf.about_blurb}</p>
                                     <h2>{aboutData.acf.experiences_heading}</h2>
@@ -75,26 +79,27 @@ const AboutModal = ({ onClose }) => {
                                     </ul>
                                 </div>
                                 <div className={tab === 2 ? "show-tab" : "hide-tab"}>
-                                    <h2>{aboutData.acf.stack_heading}</h2>
-                                    <ul>
+                                    {/* <h2>{aboutData.acf.stack_heading}</h2> */}
+                                    <div>
                                     {stackData.map((stack, index) => (
-                                        <li key={index}>
+                                        <div key={index}>
                                             <h3>{stack.title.rendered}</h3>
                                             <p>{stack.acf.stack}</p>
-                                        </li>
+                                        </div>
                                         ))}
-                                    </ul>
+                                    </div>
                                 </div>
                                 <div className={tab === 3 ? "show-tab" : "hide-tab"}>
-                                    <h2>{aboutData.acf.faq_heading}</h2>
-                                    <ul>
+                                    {/* <h2>{aboutData.acf.faq_heading}</h2> */}
                                     {aboutData.acf.faq.map((faqItem, index) => (
-                                        <div key={index}>
-                                            <h3>{faqItem.question}</h3>
+                                        <div className='faq-section' key={index}>
+                                            <div className='question-heading'>
+                                                <h3>{faqItem.question}</h3>
+                                                <IoIosArrowDown className='down-arrow'/>
+                                            </div>
                                             <p>{faqItem.answer}</p>
                                         </div>
                                     ))}
-                                    </ul>
                                 </div>
                             </div>
                         </section>
