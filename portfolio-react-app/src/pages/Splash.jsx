cfd8ef01269e7db72e4d7b97dbb482242c03c0de
veronicawong3042 <150846086+veronicawong3042@users.noreply.github.com>
2024-03-14 16:38:46 -0700

    recoloured all content

--- portfolio-react-app/src/pages/Splash.jsx
+++ portfolio-react-app/src/pages/Splash.jsx
@@ -5,9 +5,11 @@ const SplashPage = ({ onStart }) => {
     return (
         <div className="splash-page">
             <img src={mySpriteImage} alt="me" />
-            <h1>hi! i'm veronica</h1>
-            <p>click start to enter my desktop</p>
-            <button onClick={onStart}>start</button>
+            <div className='splash-content'>
+                <h1>hi! i'm veronica</h1>
+                <p>click start to enter my desktop</p>
+                <button onClick={onStart}>start</button>
+            </div>
         </div>
     );
 }
