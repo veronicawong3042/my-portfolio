4d0bf0d42ed5931ea9c468135da52f7b01d5c00b
veronicawong3042 <150846086+veronicawong3042@users.noreply.github.com>
2024-03-30 17:16:27 -0700

    hover fx, loading fix, header fix, splash change

--- portfolio/src/components/ThemeToggle.jsx
+++ portfolio/src/components/ThemeToggle.jsx
@@ -1,15 +1,19 @@
-import { useContext, useEffect } from "react";
+import { useContext } from "react";
 import { ThemeContext } from "../context/ThemeContext";
 
 const ThemeToggle = () => {
     const { theme, setTheme } = useContext(ThemeContext);
     
-    const handleDarkBlueToggle = () => {
-        setTheme(theme === 'light-blue' || theme === 'dark-blue' ? 'dark-blue' : 'dark-blue')
+    const handleLightBlueToggle = () => {
+        setTheme(theme === 'dark-blue' || theme === 'light-blue' || theme === 'green' ? 'light-blue' : 'light-blue')
     };
 
-    const handleLightBlueToggle = () => {
-        setTheme(theme === 'dark-blue' || theme === 'light-blue' ? 'light-blue' : 'light-blue')
+    const handleDarkBlueToggle = () => {
+        setTheme(theme === 'dark-blue' || theme === 'light-blue' || theme === 'green' ? 'dark-blue' : 'dark-blue')
+    };
+
+    const handleGreenToggle = () => {
+        setTheme(theme === 'dark-blue' || theme === 'light-blue' || theme === 'green' ? 'green' : 'green')
     };
     
     return (
@@ -18,6 +22,9 @@ const ThemeToggle = () => {
                 <circle cx="11.998" cy="11.998" fillRule="nonzero" r="9.998"/>
             </svg>
             <svg className="light-blue-dot" onClick={handleLightBlueToggle} clipRule="evenodd" fillRule="evenodd" strokeLinejoin="round" strokeMiterlimit="2" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                <circle cx="11.998" cy="11.998" fillRule="nonzero" r="9.998"/>
+            </svg>
+            <svg className="green-dot" onClick={handleGreenToggle} clipRule="evenodd" fillRule="evenodd" strokeLinejoin="round" strokeMiterlimit="2" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                 <circle cx="11.998" cy="11.998" fillRule="nonzero" r="9.998"/>
             </svg>
         </div>
--- portfolio/src/pages/Splash.jsx
+++ portfolio/src/pages/Splash.jsx
@@ -1,17 +1,30 @@
-import React from 'react';
 import mySpriteImage from '../images/my-sprite.png';
+import { BsArrowRightCircleFill } from "react-icons/bs";
 
 const SplashPage = ({ onStart }) => {
+
     return (
         <div className="splash-page">
             <img src={mySpriteImage} alt="me" />
             <div className='splash-content'>
-                <h1>hi! i'm veronica</h1>
-                <p>click start to enter my desktop</p>
-                <button onClick={onStart}>start</button>
+                <h1>Portfolio</h1>
+                <p className='username'>veronicawong</p>  
+                <div className="password">
+                    <div className='dots'>
+                        <span class="dot"></span>
+                        <span class="dot"></span>
+                        <span class="dot"></span>
+                        <span class="dot"></span>
+                        <span class="dot"></span>
+                        <span class="dot"></span>
+                        <span class="dot"></span>
+                        <span class="dot"></span>
+                    </div>
+                    <BsArrowRightCircleFill className='arrow-button' onClick={onStart} />
+                </div>
             </div>
         </div>
-    );
-}
+    )
+        }
 
 export default SplashPage;
